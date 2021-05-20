7e3c6577753add72264ec255575346bbe5e544fe
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2021-05-20 20:42:37 +0530

    Assignment
    Question:
    Write a program in JavaScript to get the sum output using the inputs.
    Must work for all examples , add user input as well.
    
    Example 1:
    arr1 = [2, 6, 8];
    arr2 = [4, 3, 4, 4];
    output = [4, 6, 1, 2];
    
    Example 2:
    arr1 = [20, 16, 33, 11];
    arr2 = [5, -7, 6];
    output = [2, 2, 3, 7, 7];

---
+++ sum_output_assignment.js
@@ -0,0 +1,40 @@
+
+// var a1 = [2,7,8].reverse();
+// var a2 = [4,2,3,4].reverse();
+var output=[], carry=0;
+var arrString1 = prompt("Enter the array 1 (For e.g [1,2,3,4] So enter 1,2,3,4): ");
+var arrString2 = prompt("Enter the array 2 (For e.g [1,2,3,4] So enter 1,2,3,4): ");
+var a1 = arrString1.split(',').reverse();
+var a2 = arrString2.split(',').reverse();
+
+
+  if(a1.length > a2.length){
+    //swapping the array
+    temp = a1;
+    a1=a2;
+    a2=temp;
+}
+
+//starting with the smallest array first element
+for(var i =0; i<a1.length ; i++){
+    //adding the same index length number
+    var sum = Number(a1[i])+Number(a2[i])+carry;
+    var unit = sum%10;
+    output.unshift(unit);
+    carry = Math.floor(sum/10)
+}
+//left element of the array whose size is bigged
+for(var j=a1.length; j<a2.length; j++){
+    var sum = Number(a2[j]) + carry;
+    var unit = sum%10;
+    output.unshift(unit);
+    carry = Math.floor(sum/10);
+}
+//checking if the carry is left so displaying it as unit digit
+while(carry >0){
+    output.unshift(carry%10);
+    carry = Math.floor(carry/10);
+}
+
+//output array is the final array
+console.log(output);
