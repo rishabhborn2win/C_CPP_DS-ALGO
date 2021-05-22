df84f03d75cb532ebc7d984d64ed52e0fc0a810f
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2021-05-22 10:44:06 +0530

    Now it has been made working for negative inputs

--- sum_output_assignment.js
+++ sum_output_assignment.js
@@ -1,11 +1,11 @@
 
-// var a1 = [2,7,8].reverse();
-// var a2 = [4,2,3,4].reverse();
+var a1 = [-22,7,-89].reverse();
+var a2 = [-22,7,-89].reverse();
 var output=[], carry=0;
-var arrString1 = prompt("Enter the array 1 (For e.g [1,2,3,4] So enter 1,2,3,4): ");
-var arrString2 = prompt("Enter the array 2 (For e.g [1,2,3,4] So enter 1,2,3,4): ");
-var a1 = arrString1.split(',').reverse();
-var a2 = arrString2.split(',').reverse();
+// var arrString1 = prompt("Enter the array 1 (For e.g [1,2,3,4] So enter 1,2,3,4): ");
+// var arrString2 = prompt("Enter the array 2 (For e.g [1,2,3,4] So enter 1,2,3,4): ");
+// var a1 = arrString1.split(',').reverse();
+// var a2 = arrString2.split(',').reverse();
 
 
   if(a1.length > a2.length){
@@ -21,19 +21,31 @@ for(var i =0; i<a1.length ; i++){
     var sum = Number(a1[i])+Number(a2[i])+carry;
     var unit = sum%10;
     output.unshift(unit);
-    carry = Math.floor(sum/10)
+    if(sum>0){
+        carry = Math.floor(sum/10);
+        }else{
+            carry = Math.ceil(sum/10);
+        }
 }
 //left element of the array whose size is bigged
 for(var j=a1.length; j<a2.length; j++){
     var sum = Number(a2[j]) + carry;
     var unit = sum%10;
     output.unshift(unit);
+    if(sum>0){
     carry = Math.floor(sum/10);
+    }else{
+        carry = Math.ceil(sum/10);
+    }
 }
 //checking if the carry is left so displaying it as unit digit
-while(carry >0){
+while(carry > 0){
     output.unshift(carry%10);
     carry = Math.floor(carry/10);
+}
+while(carry < 0){
+    output.unshift(carry%10);
+    carry = Math.ceil(carry/10);
 }
 
 //output array is the final array
